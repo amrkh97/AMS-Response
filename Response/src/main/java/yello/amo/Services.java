ea5c8e45457b64c47a27dd177d4adad342d989fe
amrkh97 <amrkh97@gmail.com>
2019-07-18 11:35:02 +0200

    Modified Appropriate Return Errors

--- Response/src/main/java/yello/amo/Services.java
+++ Response/src/main/java/yello/amo/Services.java
@@ -9,6 +9,7 @@ import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import Models.IncidentResponse.IncidentResponse;
 import Models.IncidentResponse.SearchResponseStatus;
+import Models.IncidentResponse.ServerResponse;
 import Models.IncidentResponse.UpdateResponseStatus;
 import BLL.ResponseManager;
 
@@ -26,20 +27,28 @@ public class Services {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response InsertResponse(IncidentResponse Incident) throws Exception {
+		ServerResponse response = new ServerResponse();
 		if (Incident.getVin() == 0) {
-			return Response.ok(" No Car ID Provided ").build();
+			response.setResponseMsg(" No Car ID Provided ");
+			return Response.status(401).entity(response).build();
 		} else if (Incident.getStartLocID() == 0) {
-			return Response.ok(" No Start Location ID Provided ").build();
+			response.setResponseMsg(" No Start Location ID Provided ");
+			return Response.status(402).entity(response).build();
 		} else if (Incident.getPickLocID() == 0) {
-			return Response.ok(" No Pick Up Locaiton ID Provided ").build();
+			response.setResponseMsg(" No Pick Up Locaiton ID Provided ");
+			return Response.status(403).entity(response).build();
 		} else if (Incident.getDropLocID() == 0) {
-			return Response.ok(" No Drop Locaiton ID Provided ").build();
+			response.setResponseMsg(" No Drop Locaiton ID Provided ");
+			return Response.status(405).entity(response).build();
 		} else if (Incident.getDestLocID() == 0) {
-			return Response.ok(" No Destination Locaiton ID Provided ").build();
+			response.setResponseMsg(" No Destination Locaiton ID Provided ");
+			return Response.status(406).entity(response).build();
 		} else if (Incident.getiSQN() == 0) {
-			return Response.ok(" No Incident Squance Number Provided ").build();
+			response.setResponseMsg(" No Incident Squance Number Provided ");
+			return Response.status(407).entity(response).build();
 		} else if (Incident.getAlarmLevelID() == 0) {
-			return Response.ok(" No Alarm Level ID Provided ").build();
+			response.setResponseMsg(" No Alarm Level ID Provided ");
+			return Response.status(408).entity(response).build();
 		} else {
 			return Response.ok(ResponseManager.addIncidentResponse(Incident)).build();
 		}
@@ -50,8 +59,10 @@ public class Services {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response SearchResponseStatus(SearchResponseStatus Incident) {
+		ServerResponse response = new ServerResponse();
 		if (Incident.getSequanceNumber() == 0) {
-			return Response.ok(" No Response Squance Number Provided ").build();
+			response.setResponseMsg(" No Response Squance Number Provided ");
+			return Response.status(401).entity(response).build();
 		} else {
 			return Response.ok(ResponseManager.SearchResponseStatus(Incident)).build();
 		}
@@ -62,10 +73,13 @@ public class Services {
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response UpdateResponseStatus(UpdateResponseStatus Incident) {
+		ServerResponse response = new ServerResponse();
 		if (Incident.getSequanceNumber() == 0) {
-			return Response.ok(" No Response Squance Number Provided ").build();
+			response.setResponseMsg(" No Response Squance Number Provided ");
+			return Response.status(401).entity(response).build();
 		} else if (Incident.getResponseStatus() == null || Incident.getResponseStatus().equals("")) {
-			return Response.ok(" No Response Status Provided ").build();
+			response.setResponseMsg(" No Response Status Provided ");
+			return Response.status(402).entity(response).build();
 		} else {
 			return Response.ok(ResponseManager.UpdateResponseStatus(Incident)).build();
 		}
